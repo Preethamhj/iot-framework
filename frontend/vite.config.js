cc97cc0610ee2a5b13ed670689ea02c9ec452ac6
pratheeksha29-20 <pratheekshadevaraj.phd.123@gmail.com>
2025-11-22 00:44:20 +0530

    d3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,19 +2,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-// ⚠️ FIX: Change to a NAMED import to correctly get the factory function
 
-
-// https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react(), tailwindcss()
-=======
   plugins: [
     react(), 
     tailwindcss()
     // The usage here remains the same, calling the imported function
 
->>>>>>> 5f737447316f39d80edf990ed6829c9b1bb3444b
   ],
 })
